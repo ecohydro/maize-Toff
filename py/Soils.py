6f17911002b61dfe791b3d1849be502a4d965c71
Kelly Caylor <caylor@ucsb.edu>
2019-07-08 12:34:50 -0700

    Defined soil classes and psi-theta relationships

--- py/Soils.py
+++ py/Soils.py
@@ -1,98 +1,100 @@
 
 
-rho = 1000  # density of water in kg/m^3
-g = 9.8     # acceleration of gravity in m/s^2
+#%% Set parameters related to soils and siginificant digits
+rho = 1000      # density of water in kg/m^3
+g = 9.8         # acceleration of gravity in m/s^2
+PRECISION =    # Number of decimal places of precision in calculations (default is 2)
 
 #%% DATA FROM CLAPP AND HORBERGER (C&H) 1978, Table 2:
 
 soils = {
     'sand':{
         'b': 4.05,
-        'Psi_S': 12.1,  # saturated water tension, cm
-        'Psi_l': 4.66,  # leakage water tension, cm
-        'n': 0.395,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 1.056,    # saturated hydraulic conductivity, cm/min
-        'S': 1.52       # sorptivity, cm/min^1/2    
+        'Psi_S_cm': 12.1,   # saturated water tension, cm
+        'Psi_l': 4.66,      # leakage water tension, cm
+        'n': 0.395,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 1.056,        # saturated hydraulic conductivity, cm/min
+        'S': 1.52           # sorptivity, cm/min^1/2    
     },
     'loamy sand':{
         'b': 4.38,
-        'Psi_S': 9.0,  # saturated water tension, cm
-        'Psi_l': 2.38,  # leakage water tension, cm
-        'n': 0.410,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.938,    # saturated hydraulic conductivity, cm/min
-        'S': 1.04       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 9.0,    # saturated water tension, cm
+        'Psi_l': 2.38,      # leakage water tension, cm
+        'n': 0.410,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.938,        # saturated hydraulic conductivity, cm/min
+        'S': 1.04           # sorptivity, cm/min^1/2  
     },
     'sandy loam':{
         'b': 4.90,
-        'Psi_S': 21.8,  # saturated water tension, cm
-        'Psi_l': 9.52,  # leakage water tension, cm
-        'n': 0.435,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.208,    # saturated hydraulic conductivity, cm/min
-        'S': 1.03       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 21.8,   # saturated water tension, cm
+        'Psi_l': 9.52,      # leakage water tension, cm
+        'n': 0.435,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.208,        # saturated hydraulic conductivity, cm/min
+        'S': 1.03           # sorptivity, cm/min^1/2  
     },
     'silt loam':{
         'b': 5.30,
-        'Psi_S': 78.6,  # saturated water tension, cm
-        'Psi_l': 75.3,  # leakage water tension, cm
-        'n': 0.485,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.0432,    # saturated hydraulic conductivity, cm/min
-        'S': 1.26       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 78.6,   # saturated water tension, cm
+        'Psi_l': 75.3,      # leakage water tension, cm
+        'n': 0.485,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.0432,       # saturated hydraulic conductivity, cm/min
+        'S': 1.26           # sorptivity, cm/min^1/2  
     },
     'loam':{
         'b': 5.39,
-        'Psi_S': 47.8,  # saturated water tension, cm
-        'Psi_l': 20.0,  # leakage water tension, cm
-        'n': 0.451,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.0417,    # saturated hydraulic conductivity, cm/min
-        'S': 0.693       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 47.8,   # saturated water tension, cm
+        'Psi_l': 20.0,      # leakage water tension, cm
+        'n': 0.451,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.0417,       # saturated hydraulic conductivity, cm/min
+        'S': 0.693          # sorptivity, cm/min^1/2  
     },
     'sandy clay loam':{
         'b': 7.12,
-        'Psi_S': 29.9,  # saturated water tension, cm
-        'Psi_l': 11.7,  # leakage water tension, cm
-        'n': 0.420,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.0378,    # saturated hydraulic conductivity, cm/min
-        'S': 0.488       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 29.9,   # saturated water tension, cm
+        'Psi_l': 11.7,      # leakage water tension, cm
+        'n': 0.420,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.0378,       # saturated hydraulic conductivity, cm/min
+        'S': 0.488          # sorptivity, cm/min^1/2  
     },
     'silty clay loam':{
         'b': 7.75,
-        'Psi_S': 35.6,  # saturated water tension, cm
-        'Psi_l': 19.7,  # leakage water tension, cm
-        'n': 0.477,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.0102,    # saturated hydraulic conductivity, cm/min
-        'S': 0.310       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 35.6,   # saturated water tension, cm
+        'Psi_l': 19.7,      # leakage water tension, cm
+        'n': 0.477,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.0102,       # saturated hydraulic conductivity, cm/min
+        'S': 0.310          # sorptivity, cm/min^1/2  
     },
     'clay loam':{
         'b': 8.52,
-        'Psi_S': 63.0,  # saturated water tension, cm
-        'Psi_l': 48.1,  # leakage water tension, cm
-        'n': 0.476,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.0147,    # saturated hydraulic conductivity, cm/min
-        'S': 0.537       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 63.0,   # saturated water tension, cm
+        'Psi_l': 48.1,      # leakage water tension, cm
+        'n': 0.476,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.0147,       # saturated hydraulic conductivity, cm/min
+        'S': 0.537          # sorptivity, cm/min^1/2  
     },
     'sandy clay':{
         'b': 10.4,
-        'Psi_S': 15.3,  # saturated water tension, cm
-        'Psi_l': 8.18,  # leakage water tension, cm
-        'n': 0.426,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.0130,    # saturated hydraulic conductivity, cm/min
-        'S': 0.223       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 15.3,   # saturated water tension, cm
+        'Psi_l': 8.18,      # leakage water tension, cm
+        'n': 0.426,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.0130,       # saturated hydraulic conductivity, cm/min
+        'S': 0.223          # sorptivity, cm/min^1/2  
     },
     'silty clay':{
         'b': 10.4,
-        'Psi_S': 49.0,  # saturated water tension, cm
-        'Psi_l': 23.0,  # leakage water tension, cm
-        'n': 0.492,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.0062,    # saturated hydraulic conductivity, cm/min
-        'S': 0.242       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 49.0,   # saturated water tension, cm
+        'Psi_l': 23.0,      # leakage water tension, cm
+        'n': 0.492,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.0062,       # saturated hydraulic conductivity, cm/min
+        'S': 0.242          # sorptivity, cm/min^1/2  
     },
     'clay':{
         'b': 11.4,
-        'Psi_S': 40.5,  # saturated water tension, cm
-        'Psi_l': 24.3,  # leakage water tension, cm
-        'n': 0.482,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
-        'Ks': 0.0077,   # saturated hydraulic conductivity, cm/min
-        'S': 0.268       # sorptivity, cm/min^1/2  
+        'Psi_S_cm': 40.5,   # saturated water tension, cm
+        'Psi_l': 24.3,      # leakage water tension, cm
+        'n': 0.482,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
+        'Ks': 0.0077,       # saturated hydraulic conductivity, cm/min
+        'S': 0.268          # sorptivity, cm/min^1/2  
     }
 }
 
@@ -101,7 +103,7 @@ soils = {
 class Soil():
     """ Defines a soil object based on either passed parameters
     or a soil texture class corresponding to the textures defined in 
-    Clapp & Hornberger, 1978, Table 2.
+    Clapp & Hornberger (C&H), 1978, Table 2.
 
     Usage: Soil(texture, params)
         Notes: 
@@ -116,7 +118,7 @@ class Soil():
              "Silty Clay", "Clay"]
         
         params = dictionary containing values for the soil parameters:
-            'params':{
+            params = {
                 'b': 11.4,
                 'Psi_S': 40.5,  # saturated water tension, cm
                 'Psi_l': 24.3,  # leakage water tension, cm
@@ -124,31 +126,96 @@ class Soil():
                 'Ks': 0.0077,   # saturated hydraulic conductivity, cm/min
                 'S': 0.268       # sorptivity, cm/min^1/2  
             }
+    
+    Note: In C&H 1978, soil water retention relationships were defined according to _tensions_. 
+    These tensions are specified as lengths, and are always _positive_ 
+    (tension, like depth has an implied relationship to zero).
+
+    To convert a tension, Psi_cm, (positive quantity of length) into a water potential, Psi_Pa, 
+    (negative measure of energy density per unit volume, or Pa), you do the following:
+
+    Psi_Pa = -1 * Psi_cm * rho * g
+
+    This conversion is done during initiation of the soil class.
+
     """
     def __init__(self, texture=None, params=None):
-        if texture:
-            self.b = soils[texture]["b"]
-            self.Psi_S = soils[texture]["Psi_S"]
-            self.Psi_l = soils[texture]["Psi_l"]
-            self.n = soils[texture]["n"]
-            self.Ks = soils[texture]["Ks"]
-            self.S = soils[texture]["S"]
-        elif params:
-            self.b = params["b"]
-            self.Psi_S = params["Psi_S"]
-            self.Psi_l = params["Psi_l"]
-            self.n = params["n"]
-            self.Ks = params["Ks"]
-            self.S = params["S"]
+        """ Initializes a soil object.
+
+        The init function requires _either_ a soil texture or a params dictionary
+        (see class description)
+        
+        """
+        self._valid_params = set(['b', 'Psi_S', 'Psi_l', 'n', 'Ks', 'S'])
+        self._required_params = set(['b', 'Psi_S', 'n', 'Ks'])
+        if texture: # If this class is instanced with a specific USDA soil texture.
+            texture = texture.lower() # Force the soil texture category to lower case
+            # Assign texture parameters based on the appropriate soil class:
+            for attr, val in soils[texture].items():
+                setattr(self, attr, val)
+        elif params: # If the class is instanced with a set of soil parameters
+            for attr, val in params.items():
+                # Only include valid soil parameters
+                if attr in self._valid_params:  
+                    setattr(self, attr, val)
+            # Check that all required parameters have been set
+            if not self._required_params.issubset(self.__dict__.keys()):
+                missing = self._required_params.difference(self.__dict__.keys())
+                raise AttributeError("Missing required parameters, {list}".format(list=missing))
+        else: 
+            raise AttributeError("Must pass either a soil texture or dict of parameters")
+        
+        # Set Psi_S (MPa) from Psi_S_cm (cm). Assumes that Psi_S_cm is positive (as it should be!)
+        self.Psi_S_MPa = -self.Psi_S_cm * rho * g / 10E6
 
     def psi(self,theta):
         """ Return water potential in Pa based 
         on volumetric soil water content in m^3/m^3
+
+        Note: Assumes that Psi is a water potential, and therefore Psi < 0 for unsaturated soils!
 
         Usage: psi(theta):
         
             theta = soil water content [m^3/m^3]
         
         """
+        if theta > self.n or theta <= 0:
+            raise ValueError("theta, {theta}, must be be in the interval (0,{n}]".format(
+                theta=theta,
+                n=self.n
+            ))            
+        return round(self.Psi_S_MPa * pow(self.n/theta,self.b),PRECISION)
+    
+    def theta(self,psi):
+        """ Return a volumetric water content in m^3/m^3 
+        based on a given water potential (MPa)
 
-        return self.n
+        Note: 
+        Usage: theta(psi):
+
+            psi = soil water potential [MPa]
+        
+        """
+        if psi > 0:
+            raise ValueError("psi, {psi}, must be less than or equal to zero.".format(psi=psi))
+        # Ensure result is rounded to correct precision and that we do not exceed porosity
+        return min([round((self.n * pow(psi/self.Psi_S_MPa, 1/-self.b)),PRECISION), self.n])
+
+    def s(self,theta):
+        """ Return a relative soil moisture value, s [0-1]
+        given a volumetric water content [m^3/m^3]
+
+        Usage: s(theta):
+
+            theta = volumetric water content [m^3/m^3]
+
+        Note: theta must be in the interval 0-n (porosity)
+        
+        """
+        if 0 < theta <= self.n:
+            return round(theta/self.n, PRECISION)
+        else:
+            raise ValueError("theta, {theta}, must be be in the interval (0,{n}]".format(
+                theta=theta,
+                n=self.n
+            ))
