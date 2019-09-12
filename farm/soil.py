e31155000964d53be6b6d8d37c57dcd7f31e8e5c
Natasha Krell <nkrell@ucsb.edu>
2019-09-12 12:19:39 -0700

    Edited TODOs within objects

--- farm/soil.py
+++ farm/soil.py
@@ -10,7 +10,7 @@ The example module supplies one function, factorial().  For example,
 #%% Set parameters related to soils and siginificant digits
 rho = 1000      # density of water in kg/m^3
 g = 9.8         # acceleration of gravity in m/s^2
-PRECISION = 2    # Number of decimal places of precision in calculations (default is 2)
+PRECISION = 2   # Number of decimal places of precision in calculations (default is 2)
 
 #%% DATA FROM CLAPP AND HORBERGER (C&H) 1978, Table 2:
 
@@ -25,8 +25,8 @@ soils = {
     },
     'loamy sand':{
         'b': 4.38,
-        'Psi_S_cm': 9.0,    # saturated water tension, cm
-        'Psi_l_cm': 2.38,      # leakage water tension, cm
+        'Psi_S_cm': 9.0,    # saturated water tension, cm 
+        'Psi_l_cm': 2.38,   # leakage water tension, cm
         'n': 0.410,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.938,        # saturated hydraulic conductivity, cm/min
         'S': 1.04           # sorptivity, cm/min^1/2  
@@ -34,7 +34,7 @@ soils = {
     'sandy loam':{
         'b': 4.90,
         'Psi_S_cm': 21.8,   # saturated water tension, cm
-        'Psi_l_cm': 9.52,      # leakage water tension, cm
+        'Psi_l_cm': 9.52,   # leakage water tension, cm
         'n': 0.435,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.208,        # saturated hydraulic conductivity, cm/min
         'S': 1.03           # sorptivity, cm/min^1/2  
@@ -42,7 +42,7 @@ soils = {
     'silt loam':{
         'b': 5.30,
         'Psi_S_cm': 78.6,   # saturated water tension, cm
-        'Psi_l_cm': 75.3,      # leakage water tension, cm
+        'Psi_l_cm': 75.3,   # leakage water tension, cm
         'n': 0.485,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.0432,       # saturated hydraulic conductivity, cm/min
         'S': 1.26           # sorptivity, cm/min^1/2  
@@ -50,15 +50,15 @@ soils = {
     'loam':{
         'b': 5.39,
         'Psi_S_cm': 47.8,   # saturated water tension, cm
-        'Psi_l_cm': 20.0,      # leakage water tension, cm
+        'Psi_l_cm': 20.0,   # leakage water tension, cm
         'n': 0.451,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.0417,       # saturated hydraulic conductivity, cm/min
         'S': 0.693          # sorptivity, cm/min^1/2  
     },
     'sandy clay loam':{
         'b': 7.12,
-        'Psi_S_cm': 29.9,   # saturated water tension, cm
-        'Psi_l_cm': 11.7,      # leakage water tension, cm
+        'Psi_S_cm': 29.9,   # saturated water tension, value cm
+        'Psi_l_cm': 11.7,   # leakage water tension, cm
         'n': 0.420,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.0378,       # saturated hydraulic conductivity, cm/min
         'S': 0.488          # sorptivity, cm/min^1/2  
@@ -66,7 +66,7 @@ soils = {
     'silty clay loam':{
         'b': 7.75,
         'Psi_S_cm': 35.6,   # saturated water tension, cm
-        'Psi_l_cm': 19.7,      # leakage water tension, cm
+        'Psi_l_cm': 19.7,   # leakage water tension, cm
         'n': 0.477,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.0102,       # saturated hydraulic conductivity, cm/min
         'S': 0.310          # sorptivity, cm/min^1/2  
@@ -74,7 +74,7 @@ soils = {
     'clay loam':{
         'b': 8.52,
         'Psi_S_cm': 63.0,   # saturated water tension, cm
-        'Psi_l_cm': 48.1,      # leakage water tension, cm
+        'Psi_l_cm': 48.1,   # leakage water tension, cm
         'n': 0.476,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.0147,       # saturated hydraulic conductivity, cm/min
         'S': 0.537          # sorptivity, cm/min^1/2  
@@ -82,7 +82,7 @@ soils = {
     'sandy clay':{
         'b': 10.4,
         'Psi_S_cm': 15.3,   # saturated water tension, cm
-        'Psi_l_cm': 8.18,      # leakage water tension, cm
+        'Psi_l_cm': 8.18,   # leakage water tension, cm
         'n': 0.426,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.0130,       # saturated hydraulic conductivity, cm/min
         'S': 0.223          # sorptivity, cm/min^1/2  
@@ -90,7 +90,7 @@ soils = {
     'silty clay':{
         'b': 10.4,
         'Psi_S_cm': 49.0,   # saturated water tension, cm
-        'Psi_l_cm': 23.0,      # leakage water tension, cm
+        'Psi_l_cm': 23.0,   # leakage water tension, cm
         'n': 0.492,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.0062,       # saturated hydraulic conductivity, cm/min
         'S': 0.242          # sorptivity, cm/min^1/2  
@@ -98,7 +98,7 @@ soils = {
     'clay':{
         'b': 11.4,
         'Psi_S_cm': 40.5,   # saturated water tension, cm
-        'Psi_l_cm': 24.3,      # leakage water tension, cm
+        'Psi_l_cm': 24.3,   # leakage water tension, cm
         'n': 0.482,         # porosity, cm^3/cm^3 (is Psi_S) in C&H,
         'Ks': 0.0077,       # saturated hydraulic conductivity, cm/min
         'S': 0.268          # sorptivity, cm/min^1/2  
@@ -131,7 +131,7 @@ class Soil():
                 'Psi_l': 24.3,  # leakage water tension, cm
                 'n': 0.482,     # porosity, cm^3/cm^3 (is Psi_S) in C&H,
                 'Ks': 0.0077,   # saturated hydraulic conductivity, cm/min
-                'S': 0.268       # sorptivity, cm/min^1/2  
+                'S': 0.268      # sorptivity, cm/min^1/2  
             }
     
     Note: In C&H 1978, soil water retention relationships were defined according to _tensions_. 
@@ -184,8 +184,8 @@ class Soil():
         # on p.714, and equation 15. 
         self.sfc = pow(0.05/60/24/self.Ks,1/(2*self.b+3))
         # Hygroscopic point is when soil is so dry no further evaporation will occur.
-        self.sh = self.s(self.theta(-12))               # Hygroscopic point in relative soil moisture [0-1]
-        self.nZr = None
+        self.sh = self.s(self.theta(-12))               # Hygroscopic point in relative soil moisture [0-1] # TODO: We also set this to -100 for testing
+        self.nZr = None                                 # TODO: Hygroscopic point is a wonky parameter stuck in the middle code.. consider setting elsewhere
 
     def _check_nZr(self):
         error = "Error: Calculation depends on value of self.nZr before calling self.set_nZr"
@@ -225,7 +225,8 @@ class Soil():
         if psi > 0:
             raise ValueError("psi, {psi}, must be less than or equal to zero.".format(psi=psi))
         # Ensure result is rounded to correct precision and that we do not exceed porosity
-        return min([round((self.n * pow(psi/self.Psi_S_MPa, 1/-self.b)),PRECISION), self.n])
+        return min([round((self.n * pow(psi/self.Psi_S_MPa, 1/-self.b)),PRECISION), self.n]) 
+
 
     def s(self,theta):
         """ Return a relative soil moisture value, s [0-1]
@@ -259,7 +260,7 @@ class Soil():
                 self.nZr = self.n * plant.Zr
         """
         self.nZr = self.n * plant.Zr 
-        return self.nZr #TODO: Do we need to return this value
+        return self.nZr 
     
     def calc_Q(self,s,units='mm/day'):
         """ Determines runoff as a function of relative soil moisture
@@ -324,4 +325,3 @@ class Soil():
 if __name__ == "__main__":
     import doctest
     doctest.testmod()  
-
