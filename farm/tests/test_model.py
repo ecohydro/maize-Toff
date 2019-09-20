4d12efb460cc8c3a66f181bfd34b0a5320346bd0
noah-de <noah.de@gmail.com>
2019-09-20 07:23:25 -0700

    Basic testing framework in place

---
+++ farm/tests/test_model.py
@@ -0,0 +1,26 @@
+"""
+Name:           test_model.py
+Compatibility:  Python 3.7
+Description:    Integreation test of the model
+
+URL:            https://github.com/ecohydro/maize-Toff
+"""
+
+import unittest
+import numpy as np
+
+from farm import Climate
+from farm import Soil
+from farm import Crop
+from farm import CropModel
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        climate = Climate()
+        soil = Soil('sand')
+        crop = Crop(soil=soil)
+        self.model = CropModel(crop=crop,soil=soil,climate=climate)
+
+    def test_cropModel(self):
+        assert(isinstance(self.model.R, np.ndarray), "R value is an ndarray")
