231668e337ba435fab927a3b1b713d5a3076999b
Natasha Krell <nkrell@ucsb.edu>
2019-09-29 17:23:47 -0400

    Clarified tests for test_sand cases

--- farm/tests/test_sand.py
+++ farm/tests/test_sand.py
@@ -14,6 +14,7 @@ from farm import Soil
 from farm import Crop
 from farm import CropModel
 
+# TODO: Consider subsuming test_sand into test_soil, and run these tests for various soil types
 class TestSand(unittest.TestCase):
 
     def setUp(self):
@@ -23,8 +24,15 @@ class TestSand(unittest.TestCase):
         self.model = CropModel(crop=crop,soil=soil,climate=climate)
 
 	# write tests
+	# 1. Is the hygroscopic soil moisture less than soil moisture at wilting point?
     def test_sh(self):
         assert self.model.soil.sh <= self.model.crop.sw, "Should be true"
 
+
+    # 2. Is field capacity less than 1?
+    def test_sfc(self):
+        assert self.model.soil.sfc <= 1, "Error: field capacity should be less than 1"
+
+    # 3. Is s* less than field capacity?
     def test_star(self):
-        assert self.model.crop.s_star <= self.model.soil.sfc, "Error: s_star is too high: it should be less than field capacity"
+        assert self.model.crop.s_star <= self.model.soil.sfc, "Error: s_star is too high; it should be less than field capacity"
