419b2ec13ffc8b8019a8f10639ca0b426c860d88
Natasha Krell <nkrell@ucsb.edu>
2020-05-11 11:21:47 -0700

    added plotting functions

--- farm/functions.py
+++ farm/functions.py
@@ -26,6 +26,9 @@ import numpy as np
 import copy
 from datetime import datetime
 from dateutil.relativedelta import *
+import matplotlib.pyplot as plt
+import seaborn as sns
+import proplot as plot
 import functools
 from .climate import Climate
 from .model import CropModel
@@ -191,3 +194,131 @@ def calc_yield(stress=None, max_yield = 4680):
                 stress=stress))
     
     return yield_kg_ha
+
+# TODO: Consider moving plotting functions into their own script.
+def plot_lin_regression(x_var = None, y_var = None, x_str = None, y_str = None, data = None, 
+                        x_lab = 'X label here', y_lab = 'Y label here', title = 'Title here', positive = True):
+    """ Computes linear regression between independent and dependent variable. 
+    Usage: plot_lin_regression(x_var, y_var, x_lab, y_lab, title)
+    """
+    # Define variables
+    X, y = x_var, y_var
+    
+    # Linear regression
+    denominator = X.dot(X) - X.mean() * X.sum()
+    m = ( X.dot(y) - y.mean() * X.sum() ) / denominator
+    b = (y.mean() * X.dot(X) - X.mean() * X.dot(y) ) / denominator
+
+    y_pred = m*X + b
+
+    plt.figure(figsize=(5,4))
+
+    g = sns.lmplot(x_str, y_str, data, ci=95, height=4, scatter_kws={'color':'black','alpha':0.6}) # ,, line_kws={'color': 'black'}
+    
+    # Calculate residuals
+    res = y - y_pred
+    tot = y - y.mean()
+
+    R_squared = 1 - res.dot(res) / tot.dot(tot)
+    print(R_squared)
+    print('m',m)
+    print('b',b)
+    
+    if positive == True:
+        textstr = '\n'.join((
+            r'$ y = %.2f$x' % (m, )+'+$  %2.0f$' % (b, ),
+            r'$r^2=%.2f$' % (R_squared, ))) 
+    else:
+        textstr = '\n'.join((
+        r'$ y = %.2f$x' % (m, )+'$  %2.0f$' % (b, ),
+        r'$r^2=%.2f$' % (R_squared, )))
+
+    props = dict(boxstyle='square', facecolor='lightgray', alpha=0.5)
+
+    # place a text box in upper left in axes coords
+    g.fig.text(0.25, 2.27, textstr, fontsize=10, #transform=ax.transAxes, 
+            verticalalignment='top', bbox=props)
+    
+    plt.xlabel(x_lab)
+    plt.ylabel(y_lab)
+    plt.title(title, fontweight="bold")
+
+def power_law_fit(xdat,ydat, x_lab, y_lab, title):
+    x,y = xdat, ydat
+    power_law = lambda x, a, b: a * (x**b)
+    f, axs = plot.subplots(ncols=1, nrows=2, share=0, figsize=(5,4)) 
+    
+    # Find best fit.
+    popt, pcov = curve_fit(power_law, x, y)
+    
+    # Top plot
+    # Plot data and best fit curve.
+    axs[0].plot(x, y,'ok', alpha=0.6)
+    axs[0].plot(np.sort(x), power_law(np.sort(x), *popt),'-',markersize=3,  linewidth=2.5) # like this color color=(0.2, 0.4, 0.6, 0.6)
+    
+    #r2, v1
+    residuals = y - power_law(x, *popt)
+    ss_res = np.sum(residuals**2)
+    ss_tot = np.sum((y-np.mean(y))**2)
+    r_squared = 1 - (ss_res / ss_tot)
+    r_squared
+    
+    #r2, v2
+    from sklearn.metrics import r2_score
+    r2_score(y, power_law(x, *popt), multioutput='variance_weighted')
+    
+    # Add text
+    textstr = r'$r^2=%.2f$' % (r_squared, )
+    props = dict(boxstyle='square', facecolor='lightgray', alpha=0.5)
+    axs[0].format(suptitle=title, title = textstr,titleweight='bold', titleloc='ul',
+                 ylabel=y_lab, xlabel=x_lab)
+    
+    # Bottom plot
+    axs[1].plot(residuals) #linewidth=.9
+    axs[1].format(title='Residuals', titleweight='bold',xlabel='Whatever X label',
+                 ylabel='Error') #, titleloc='ul
+    axs[0].set_xlim(min(x)-3, max(x)+10)  
+
+def polyfit(x, y, degree):
+    results = {}
+
+    coeffs = np.polyfit(x, y, degree)
+
+     # Polynomial Coefficients
+    results['polynomial'] = coeffs.tolist()
+
+    # r-squared
+    p = np.poly1d(coeffs)
+    # fit values, and mean
+    yhat = p(x)                         # or [p(z) for z in x]
+    ybar = np.sum(y)/len(y)          # or sum(y)/len(y)
+    ssreg = np.sum((yhat-ybar)**2)   # or sum([ (yihat - ybar)**2 for yihat in yhat])
+    sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
+    results['determination'] = ssreg / sstot
+
+    return results, yhat, ybar
+
+def plot_polyfit(x=None, y=None,x_lab='Seasonal rainfall (mm)',y_lab='Yield (kg/ha)',title='Polynomial fit'):
+    
+    fig, ax = plt.subplots(figsize=(5,4))
+    p = np.poly1d(np.polyfit(x, y, 3))
+    t = np.linspace(200, 876, 1000)
+
+    ax.plot(x, y, 'ok', t, p(t), '-', markersize=3, alpha=0.6, linewidth=2.5)
+
+    results, yhat, ybar = polyfit(x,y,3)
+    R_squared = results['determination']
+    textstr = r'$r^2=%.2f$' % (R_squared, )
+    props = dict(boxstyle='square', facecolor='lightgray', alpha=0.5)
+
+    fig.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=12,
+            verticalalignment='top', bbox=props)
+    
+    plt.xlabel(x_lab)
+    plt.ylabel(y_lab)
+    plt.title(title, fontweight="bold")
+
+    results['polynomial'][0]
+
+    # TODO: confidence intervals around line?
+
