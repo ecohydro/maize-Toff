b241d1b2e78ec4b167738e37f486a533e7037a26
Natasha Krell <nkrell@ucsb.edu>
2020-11-25 12:45:22 -0800

    added test to check output length

--- farm/tests/test_model.py
+++ farm/tests/test_model.py
@@ -20,7 +20,24 @@ class TestModel(unittest.TestCase):
 
     def setUp(self):
         climate = Climate()
-        
-    def test_climate_init_with_int(self):
-       pass
+        soil_l = Soil('loam')
+        crop_l = Crop(soil=soil_l)
+        self.model = CropModel(crop=crop_l,soil=soil_l,climate=climate)
     
+	# Write tests
+	# 1. Check that the output is 180 after accounting for burn in
+    def test_length_of_output(self):
+        self.model.run()
+        o = self.model.output()
+
+        start = 60 # Not sure how to get self.burn_in
+        end = start + 180 # lgp shouldn't be hard coded in
+
+        lgp = len(o[start:end])
+
+        return lgp
+
+        assert lgp == 180, "Error: Check the length of your dataframe and that burn_in time is accounted for"
+
+if __name__ == '__main__':
+   unittest.main()
